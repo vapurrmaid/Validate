c35ba43aa423e03350ebbeb833cec6b010aa34c1
Vapurrmaid <vapurrmaid@gmail.com>
2019-12-05 22:04:48 -0500

    :sparkles: Negation

--- src/Validate.ts
+++ src/Validate.ts
@@ -1,21 +1,27 @@
+import { ValidateNegation } from './ValidateNegation';
+
 /**
  * Class assisting in validation with maintainable, readable expressions.
  * Inspired by the Java Package org.apache.commons.lang3
  *
- * @see https://commons.apache.org/proper/commons-lang/javadocs/api-3.1/org/apache/commons/lang3/Validate.html
+ * @see {@link https://commons.apache.org/proper/commons-lang/javadocs/api-3.1/org/apache/commons/lang3/Validate.html|Validate}
  */
 export class Validate {
   /**
-   * Validates that the provided expression is true, otherwise throws an
-   * exception with the provided message.
-   *
-   * @param expression Expression to be validated
-   * @param message Error message
-   * @throws Throws an Error if validation does not pass
-   */
+  * Validates that the provided expression is true; otherwise throws an
+  * Error with the provided message.
+  *
+  * @param expression Expression to be validated
+  * @param message Error message
+  * @throws Error if the expression is not true
+  */
   public static isTrue(expression: boolean, message: string): void {
     if (!expression) {
       throw new Error(message);
     }
+  }
+
+  public static get not(): ValidateNegation {
+    return ValidateNegation;
   }
 }
