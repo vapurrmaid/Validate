9ea0b4f7392832a994b188c6a3f3146fcbfed619
Vapurrmaid <vapurrmaid@gmail.com>
2019-12-05 22:46:35 -0500

    :sparkles: Add inclusiveBetween`

--- src/Validate.test.ts
+++ src/Validate.test.ts
@@ -1,6 +1,36 @@
 import { Validate } from './Validate';
 
 describe('Validate', () => {
+  describe('Validate.inclusiveBetween', () => {
+    it('does not throw if the value is within bounds', () => {
+      function shouldNotThrow(): void {
+        Validate.inclusiveBetween(0, 10, 5, 'test');
+      }
+      expect(shouldNotThrow).not.toThrowError();
+    });
+
+    it('does not throw if the value is on the lower bound', () => {
+      function shouldNotThrow(): void {
+        Validate.inclusiveBetween(0, 10, 0, 'test');
+      }
+      expect(shouldNotThrow).not.toThrowError();
+    });
+
+    it('does not throw if the value is on the upper bound', () => {
+      function shouldNotThrow(): void {
+        Validate.inclusiveBetween(0, 10, 10, 'test');
+      }
+      expect(shouldNotThrow).not.toThrowError();
+    });
+
+    it('throws with the provided message if the value is not within bounds', () => {
+      function shouldThrow(): void {
+        Validate.inclusiveBetween(0, 1, 2, 'test');
+      }
+      expect(shouldThrow).toThrowError('test');
+    });
+  });
+
   describe('Validate.isTrue', () => {
     it('does not throw for true expressions', () => {
       function shouldNotThrow(): void {
--- src/ValidateNegation.ts
+++ src/ValidateNegation.ts
@@ -1,4 +1,10 @@
 export class ValidateNegation {
+  public static inclusiveBetween(start: number, end: number, value: number, message: string): void {
+    if (!(value < start || value > end)) {
+      throw new RangeError(message);
+    }
+  }
+
   public static isTrue(expression: boolean, message: string): void {
     if (expression) {
       throw new Error(message);
