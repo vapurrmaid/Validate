677dc9348db774f1980c0502c22a8e0825e33263
G r e y <vapurrmaid@pm.me>
2019-12-18 13:26:26 -0500

    :sparkles: exclusiveBetween (#9)

--- src/Validate.test.ts
+++ src/Validate.test.ts
@@ -1,6 +1,36 @@
 import { Validate } from './Validate';
 
 describe('Validate', () => {
+  describe('Validate.exclusiveBetween', () => {
+    it('does not throw if the value is within bounds', () => {
+      function shouldNotThrow(): void {
+        Validate.exclusiveBetween(0, 10, 5, 'test');
+      }
+      expect(shouldNotThrow).not.toThrowError();
+    });
+
+    it('throws with the provided message if the value is on the lower bound', () => {
+      function shouldThrow(): void {
+        Validate.exclusiveBetween(0, 10, 0, 'test');
+      }
+      expect(shouldThrow).toThrowError('test');
+    });
+
+    it('throws with the provided message if the value is on the upper bound', () => {
+      function shouldThrow(): void {
+        Validate.exclusiveBetween(0, 10, 10, 'test');
+      }
+      expect(shouldThrow).toThrowError('test');
+    });
+
+    it('throws with the provided message if the value is not within bounds', () => {
+      function shouldThrow(): void {
+        Validate.exclusiveBetween(0, 1, 2, 'test');
+      }
+      expect(shouldThrow).toThrowError('test');
+    });
+  });
+
   describe('Validate.inclusiveBetween', () => {
     it('does not throw if the value is within bounds', () => {
       function shouldNotThrow(): void {
